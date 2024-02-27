768db3f970455d096ff4d42d32b3053ef08af6aa
Ian Page Hands <iphands@gmail.com>
2024-02-26 18:40:25 -0800

    :lipstick: refactor

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import click
 import requests
 import iperf3
 import json
@@ -5,8 +6,6 @@ import time
 import subprocess
 
 from icmplib import ping
-
-BASE = "https://unifi-controller.noir.lan"
 
 results = {}
 best = 0
@@ -42,107 +41,108 @@ channels_five = [
     165,
 ]
 
-cookies = {
-    "unifises": "W0SkTLVjwkR4fpuX7Wu5hnl8gYzVOCFR",
-    "csrf_token": "hSZBMDrKl7wRbOJjEXnwH6OV7RVxdXgC",
-}
 
-headers = {
-    "authority": "unifi-controller.noir.lan",
-    "accept": "application/json, text/plain, */*",
-    "accept-language": "en-US,en;q=0.9",
-    "content-type": "application/json;charset=utf-8",
-    "dnt": "1",
-    "origin": "https://unifi-controller.noir.lan",
-    "referer": "https://unifi-controller.noir.lan/manage/default/devices/properties/b4:fb:e4:e1:53:11/settings",
-    "sec-ch-ua": '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": '"Linux"',
-    "sec-fetch-dest": "empty",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-site": "same-origin",
-    "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
-    "x-csrf-token": "hSZBMDrKl7wRbOJjEXnwH6OV7RVxdXgC",
-}
+class Client:
+    def __init__(self, host):
+        self.host = host
+        self.cookies = {
+            "unifises": "W0SkTLVjwkR4fpuX7Wu5hnl8gYzVOCFR",
+            "csrf_token": "hSZBMDrKl7wRbOJjEXnwH6OV7RVxdXgC",
+        }
 
-
-def get_url(frag):
-    url = f"{BASE}{frag}"
-    print(f"DEBUG: url: {url}")
-    return url
+        self.headers = {
+            "authority": "unifi-controller.noir.lan",
+            "accept": "application/json, text/plain, */*",
+            "accept-language": "en-US,en;q=0.9",
+            "content-type": "application/json;charset=utf-8",
+            "dnt": "1",
+            "origin": "https://unifi-controller.noir.lan",
+            "referer": "https://unifi-controller.noir.lan/manage/default/devices/properties/b4:fb:e4:e1:53:11/settings",
+            "sec-ch-ua": '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": '"Linux"',
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-origin",
+            "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+            "x-csrf-token": "hSZBMDrKl7wRbOJjEXnwH6OV7RVxdXgC",
+        }
 
 
-def get_settings_data(chan_two, chan_five):
-    json_data = {
-        "atf_enabled": False,
-        "mesh_sta_vap_enabled": False,
-        "radio_table": [
-            {
-                "name": "ra0",
-                "ht": "20",
-                "channel": chan_two,
-                "tx_power_mode": "high",
-                "vwire_enabled": False,
-                "min_rssi_enabled": False,
-                "hard_noise_floor_enabled": False,
-                "antenna_gain": 6,
-                "sens_level_enabled": False,
-                "radio": "ng",
+
+    def get_url(self, frag):
+        url = f"{self.host}{frag}"
+        # print(f"DEBUG: url: {url}")
+        return url
+
+    def get_settings_data(self, chan_two, chan_five):
+        json_data = {
+            "atf_enabled": False,
+            "mesh_sta_vap_enabled": False,
+            "radio_table": [
+                {
+                    "name": "ra0",
+                    "ht": "20",
+                    "channel": chan_two,
+                    "tx_power_mode": "high",
+                    "vwire_enabled": False,
+                    "min_rssi_enabled": False,
+                    "hard_noise_floor_enabled": False,
+                    "antenna_gain": 6,
+                    "sens_level_enabled": False,
+                    "radio": "ng",
+                },
+                {
+                    "name": "rai0",
+                    "ht": "80",
+                    "channel": chan_five,
+                    "tx_power_mode": "high",
+                    "vwire_enabled": False,
+                    "min_rssi_enabled": False,
+                    "hard_noise_floor_enabled": True,
+                    "antenna_gain": 6,
+                    "sens_level_enabled": False,
+                    "radio": "na",
+                },
+            ],
+            "mesh_uplink_1": "",
+            "mesh_uplink_2": "",
+            "name": "",
+            "config_network": {
+                "type": "dhcp",
+                "bonding_enabled": False,
             },
-            {
-                "name": "rai0",
-                "ht": "80",
-                "channel": chan_five,
-                "tx_power_mode": "high",
-                "vwire_enabled": False,
-                "min_rssi_enabled": False,
-                "hard_noise_floor_enabled": True,
-                "antenna_gain": 6,
-                "sens_level_enabled": False,
-                "radio": "na",
-            },
-        ],
-        "mesh_uplink_1": "",
-        "mesh_uplink_2": "",
-        "name": "",
-        "config_network": {
-            "type": "dhcp",
-            "bonding_enabled": False,
-        },
-    }
+        }
 
-    return json_data
+        return json_data
 
 
-def put(frag, data):
-    # h = headers.copy()
-    # h['content-type'] = 'application/json;charset=utf-8'
-
-    response = requests.put(
-        get_url(frag),
-        cookies=cookies,
-        headers=headers,
-        json=data,
-        verify=False,
-    )
-    return response.json()
+    def put(self, frag, data):
+        response = requests.put(
+            self.get_url(frag),
+            cookies=self.cookies,
+            headers=self.headers,
+            json=data,
+            verify=False,
+        )
+        return response.json()
 
 
-def get(frag):
-    response = requests.get(
-        get_url(frag),
-        cookies=cookies,
-        headers=headers,
-        verify=False,
-    )
-    return response.json()
+    def get(self, frag):
+        response = requests.get(
+            self.get_url(frag),
+            cookies=self.cookies,
+            headers=self.headers,
+            verify=False,
+        )
+        return response.json()
 
 
-def change_chan(chan_two, chan_five):
-    return put(
-        "/api/s/default/rest/device/5de8531246e0fb00f79da35c",
-        get_settings_data(chan_two, chan_five),
-    )
+    def change_chan(self, chan_two, chan_five):
+        return self.put(
+            "/api/s/default/rest/device/5de8531246e0fb00f79da35c",
+            self.get_settings_data(chan_two, chan_five),
+        )
 
 
 def mbps(n):
@@ -170,10 +170,8 @@ def run_test(chan):
     print(f"   done (curr: {mbps(summary)}) best: {mbps(best)} on {best_chan}")
 
 
-def try_reconnect():
+def try_reconnect(uuid):
     print("-- ERROR: trying reconnect!")
-    uuid = "a04d7a33-fb6d-418c-bf4f-0e640e390e1e"  # HandsNet5
-    uuid = "d9225f27-b7b1-4181-9388-728522d7dff1"  # HandsNet
     for _ in range(0, 100):
         try:
             subprocess.check_output(["nmcli", "conn", "down", uuid])
@@ -222,10 +220,24 @@ def wait_for_ping():
             pass
 
 
-if __name__ == "__main__":
-    for chan in channels_two:
+@click.command()
+@click.option("--host", help="Base URL for host of unifi controller", required=True)
+@click.option(
+    "--mode", type=click.Choice(["2", "5"]), help="2.4Ghz or 5Ghz mode", required=True
+)
+@click.option(
+    "--nm-uuid", help="UUID of NetworkManager connection to activate", required=True
+)
+def main(host, mode, nm_uuid):
+    chans = channels_two
+    if mode == "5":
+        channels_five
+
+    client = Client(host)
+
+    for chan in chans:
         print(f"-- Changing to channel: {chan}")
-        change_chan(chan, 124)
+        client.change_chan(chan, 124)
         if not wait_for_chan(chan):
             results[chan] = False
             continue
@@ -235,3 +247,7 @@ if __name__ == "__main__":
 
     with open("results.json", "w") as f:
         f.write(json.dumps(results))
+
+
+if __name__ == "__main__":
+    main()
